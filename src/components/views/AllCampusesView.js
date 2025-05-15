ef6af90f4e44b6e46ef1cc27eaa8fcde70f6c6c2
tiffanyni47 <102348115+tiffanyni47@users.noreply.github.com>
2025-05-15 17:29:35 -0400

    Updated AllCampusesView.js
    Added button for delete campus

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -26,6 +26,7 @@ const AllCampusesView = (props) => {
           <h4>campus id: {campus.id}</h4>
           <p>{campus.address}</p>
           <p>{campus.description}</p>
+          <button onClick={() => props.deleteCampus(campus.id)}>Delete</button>
           <hr/>
         </div>
       ))}
